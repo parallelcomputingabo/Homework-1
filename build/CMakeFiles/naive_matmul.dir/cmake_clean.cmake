a9bc7385b82fb9df0c2fe3f53d53c44d902a69b0
Aslam Hossain <aslamhossain@Aslams-MacBook-Air.local>
2025-04-26 12:54:08 +0300

    student-name: Implemented naive matrix multiplication

---
+++ build/CMakeFiles/naive_matmul.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/naive_matmul.dir/src/main.cpp.o"
+  "CMakeFiles/naive_matmul.dir/src/main.cpp.o.d"
+  "naive_matmul"
+  "naive_matmul.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/naive_matmul.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
